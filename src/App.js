61f104bab5d0e4b87e80a6485e0d952e98160b04
ia popkhadze <iafofkhadze@gmail.com>
2023-05-11 18:47:58 -0700

    gurami

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Navigation from "./components/Navigation";
-import HamburgerMenu from "./components/HamburgerMenu";
+
 import Home from "./components/Home";
 import Contact from "./components/Contact";
 import About from "./components/About";
 import Shop from "./components/Shop";
 import ShopCart from "./components/ShopCart";
-import Footer from "./components/Footer";
 import Layout from "./components/Layout";
 function App() {
   return (
